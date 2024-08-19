1e1787e21b59fe1f4b7ff0c8feae252777c2171d
Ayiera-20 <ayieramyra@gmail.com>
2024-08-19 18:28:22 +0300

    feat: Add login functionality

---
+++ public/js/login.js
@@ -0,0 +1,44 @@
+// Login
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('#login-form');
+    
+    if (form) {
+        form.addEventListener('submit', async (e) => {
+            e.preventDefault();
+            
+            const username = document.getElementById('username').value;
+            const password = document.getElementById('password').value;
+            const authMsg = document.getElementById('auth-msg');
+
+            try {
+                const response = await fetch('/api/auth/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ username, password })
+                });
+
+                const data = await response.json(); // Correctly await the response data
+
+                if (!response.ok) {
+                    authMsg.textContent = data.message || 'Authentication failed';
+                } else {
+                    authMsg.textContent = data.message || 'Login successful';
+
+                    // Redirect to index.html after successful login
+                    window.location.href = '/home';
+                }
+            } catch (err) {
+                if (authMsg) {
+                    authMsg.textContent = 'Error: ' + err.message;
+                }
+            }
+        });
+    } else {
+        console.error('Login form element not found in the DOM');
+    }
+});
+
+
+
