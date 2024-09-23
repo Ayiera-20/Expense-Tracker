772e894e56c6a6379d8dbe4d68850396c16fdb4a
Ayiera-20 <ayieramyra@gmail.com>
2024-09-23 16:05:02 +0300

    feat: Implement session-based authentication and protect expense routes

--- api/routes/expensesRoutes.js
+++ api/routes/expensesRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const expensesController = require('../controllers/expensesController'); 
+const isAuthenticated = require('../middleware/authMiddleware'); 
 
 // Routes for expense management
-router.post('/api/expenses', expensesController.addExpense);
-router.get('/api/get-expenses', expensesController.getExpenses);
-router.put('/api/update-expense/:expenseId', expensesController.updateExpense);
-router.delete('/api/delete-expense/:expenseId', expensesController.deleteExpense);
+router.post('/api/expenses', isAuthenticated, expensesController.addExpense);
+router.get('/api/get-expenses', isAuthenticated, expensesController.getExpenses);
+router.put('/api/update-expense/:expenseId', isAuthenticated, expensesController.updateExpense);
+router.delete('/api/delete-expense/:expenseId', isAuthenticated, expensesController.deleteExpense);
 
 
 module.exports = router;
