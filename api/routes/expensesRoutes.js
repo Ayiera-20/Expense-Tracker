dabcf857fc53b4b28c021e3b371b3803aceb2630
Ayiera-20 <ayieramyra@gmail.com>
2024-09-26 02:28:55 +0300

    feat: remove authmiddleware

--- api/routes/expensesRoutes.js
+++ api/routes/expensesRoutes.js
@@ -4,10 +4,10 @@ const expensesController = require('../controllers/expensesController');
 const isAuthenticated = require('../middleware/authMiddleware'); 
 
 // Routes for expense management
-router.post('/api/expenses', isAuthenticated, expensesController.addExpense);
-router.get('/api/get-expenses', isAuthenticated, expensesController.getExpenses);
-router.put('/api/update-expense/:expenseId', isAuthenticated, expensesController.updateExpense);
-router.delete('/api/delete-expense/:expenseId', isAuthenticated, expensesController.deleteExpense);
+router.post('/api/expenses', expensesController.addExpense);
+router.get('/api/get-expenses', expensesController.getExpenses);
+router.put('/api/update-expense/:expenseId', expensesController.updateExpense);
+router.delete('/api/delete-expense/:expenseId', expensesController.deleteExpense);
 
 
 module.exports = router;
