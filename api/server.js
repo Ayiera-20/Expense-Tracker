28954f8182bbaa2afc5801ddbe7f3e076bb92c7f
Ayiera-20 <ayieramyra@gmail.com>
2025-04-07 17:58:39 +0300

    remove local host port server

--- api/server.js
+++ api/server.js
@@ -57,8 +57,6 @@ app.use(pagesRoutes);
 // createDatabaseAndTables();
 
 
-
-// Start server
 // const port = process.env.PORT || 4000;
 // app.listen(port, () => {
 //     console.log(`Server is running on port ${port}`);
